07ef49ee222aa932d6c1889df29c839ef66376e4
jakubbarak <jakubbarak2001@seznam.cz>
2026-09-09 08:39:18 +0200

    -added and wired up .env secret, revoked the old key that got posted on GH. -gitignore now ignores .venv and .env -added .env.example so others can wire up the app on their own API keys.
    -fixed bug which caused alternative names (like 'USA') not to be found.
    -added raise-exception for non-existent countries
    -added new function that counts points and says which country won/lost, based on population and size.

--- countries.py
+++ countries.py
@@ -1,12 +1,27 @@
 import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.getenv("MY_SECRET_KEY")
 
 def get_one_country(country):
     response = requests.get(
-        f"https://api.restcountries.com/countries/v5?q={country.lower()}",
-        headers={"Authorization": "Bearer rc_live_84c8dba8a4d94c4ba277af54202d0c0c"}
+        f"https://api.restcountries.com/countries/v5/names.common/{country.lower()}",
+        headers={"Authorization": f"Bearer {api_key}"}
     )
 
     data = response.json()
+    if not data["data"]["objects"]:
+        response = requests.get(
+            f"https://api.restcountries.com/countries/v5/names.alternates/{country.lower()}",
+            headers={"Authorization": f"Bearer {api_key}"}
+        )
+
+        data = response.json()
+        if not data["data"]["objects"]:
+            raise ValueError("This country was not found.")
     country_data = data["data"]["objects"][0]
 
     return {
@@ -19,10 +34,12 @@ def get_one_country(country):
 def get_country_data(country1, country2):
     return get_one_country(country1), get_one_country(country2)
 
+
 def ask_for_country():
-    country_name1 = input("Enter your 1st country: ")
-    country_name2 = input("Enter your 2nd country: ")
+    country_name1 = input("Enter 1st country: ")
+    country_name2 = input("Enter 2nd country: ")
     return [country_name1, country_name2]
+
 
 def compare_two_countries():
     selected_countries = ask_for_country()
@@ -41,4 +58,32 @@ def compare_two_countries():
         points[countries_api_data[1]["Name"]] += 1
     return points
 
-print(compare_two_countries())
+
+def display_final_score():
+    final_scores = compare_two_countries()
+
+    countries = list(final_scores.keys())
+    scores = list(final_scores.values())
+
+    country1 = countries[0]
+    score1 = scores[0]
+
+    country2 = countries[1]
+    score2 = scores[1]
+
+    if score1 > score2:
+        winner = country1
+        winning_score = score1
+        losing_score = score2
+        loser = country2
+    elif score2 > score1:
+        winner = country2
+        winning_score = score2
+        losing_score = score1
+        loser = country1
+    else:
+        return f"\nIt's a DRAW! {country1} and {country2} both have {score1} points."
+    return (f"\nThe winner is: {winner}! with {winning_score} points."
+            f"\nThe looser is {loser} with {losing_score} points.")
+
+print(display_final_score())
