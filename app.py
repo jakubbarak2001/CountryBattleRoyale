c2e766a42293b70bad019936f5ef318cf0aaff8c
jakubbarak <jakubbarak2001@seznam.cz>
2026-09-10 13:28:24 +0200

    index refactor + removal of user input country, now it's randomly generated.

--- app.py
+++ app.py
@@ -1,22 +1,45 @@
 from flask import Flask, render_template, request
 
-from countries import compare_two_countries
+from countries import compare_two_countries, select_and_compare_random_countries
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        result = request.form
-        country_1 = result["country1"]
-        country_2 = result["country2"]
-        try:
-            battle_data = compare_two_countries(country_1, country_2)
-        except ValueError:
-            battle_data = "One of those countries doesn't exist, try again."
-    else:
-        battle_data = "Enter the country names!"
-    return render_template('index.html', data=battle_data)
 
-if __name__ == '__main__':
+@app.route("/", methods=["GET", "POST"])
+def index():
+
+    if request.method == "GET":
+        random_countries = select_and_compare_random_countries()
+        country_names = list(random_countries.keys())
+
+        country_1 = country_names[0]
+        country_2 = country_names[1]
+
+        return render_template(
+            "index.html",
+            data=None,
+            country_1=country_1,
+            country_2=country_2
+        )
+
+    if request.method == "POST":
+        country_1 = request.form["country1"]
+        country_2 = request.form["country2"]
+        guess = request.form["guess"]
+
+        battle_data = compare_two_countries(country_1, country_2)
+
+        print(country_1)
+        print(country_2)
+        print(guess)
+
+        return render_template(
+            "index.html",
+            data=battle_data,
+            country_1=country_1,
+            country_2=country_2
+        )
+
+
+if __name__ == "__main__":
     app.run(debug=True)
