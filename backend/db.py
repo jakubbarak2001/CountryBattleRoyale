fb83d392830a97af23742eadef67f5697edd4f4b
jakubbarak <jakubbarak2001@seznam.cz>
2026-09-19 13:59:19 +0200

    feat(db): add user creation with password hashing

--- backend/db.py
+++ backend/db.py
@@ -1,8 +1,25 @@
 import os
 
 import psycopg
-from psycopg.rows import dict_row
 from argon2 import PasswordHasher
 
 password_hasher = PasswordHasher()
 
+
+def create_user(username, password, email):
+    pass_hash = password_hasher.hash(password)
+
+    with psycopg.connect(os.environ["DATABASE_URL"]) as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                """
+                INSERT INTO users (username, pass_hash, email)
+                VALUES (%s, %s, %s)
+                RETURNING id
+                """,
+                (username, pass_hash, email),
+            )
+
+            user_id = cur.fetchone()[0]
+
+    return user_id
